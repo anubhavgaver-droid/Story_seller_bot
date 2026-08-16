c6a0e16ddcd8bef01fe4decec19d4638b051a50f
anubhavgaver-droid <anubhavgaver@gmail.com>
2026-08-16 21:15:20 +0530

    Add enums import to start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums
 from pyrogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from database.db import get_story_by_title, send_log, is_user_registered, register_user
 from config import BOT_USERNAME
