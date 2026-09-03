5ee4d418f7134f45c9d70a4295736191a3c81791
anubhavgaver-droid <anubhavgaver@gmail.com>
2026-09-03 18:14:58 +0530

    Add CHANNEL_ID for database channel configuration
    Added CHANNEL_ID variable for database channel configuration.

--- config.py
+++ config.py
@@ -5,6 +5,10 @@ API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", "987654321"))
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001234567890"))
+
+# Database Channel Variable (जहाँ से फाइल्स PM में फॉरवर्ड होंगी)
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001234567890"))
+
 UPI_ID = os.environ.get("UPI_ID", "63983224472@fam")
 PORT = int(os.environ.get("PORT", "8080"))
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "YourStorySellerBot")
