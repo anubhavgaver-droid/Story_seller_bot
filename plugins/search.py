0c7db5c07d6844f221fa3b50a756e530f9361d33
anubhavgaver-droid <anubhavgaver@gmail.com>
2026-09-04 10:00:06 +0530

    Remove unused import in search.py
    Removed unused import of get_main_menu from strings.

--- plugins/search.py
+++ plugins/search.py
@@ -24,7 +24,7 @@ from database.db import (
     is_story_unlocked
 )
 from config import WEB_APP_URL, BOT_USERNAME, CHANNEL_ID
-from strings import get_text, get_main_menu  # Import Language System
+from strings import get_text  # Import Language System
 
 # State and Storage Dictionaries
 SEARCH_WAITING = {}
